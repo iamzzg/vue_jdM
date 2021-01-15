e63d9712b0d15164c4663d00e0724a0fe4c1a706
iamzzg <785864219@qq.com>
2021-01-15 21:35:45 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-01-15 20:39:54
+ * @LastEditTime: 2021-01-15 21:24:09
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue_jdm\src\App.vue
+-->
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div class="app"></div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style lang="less" scoped>
+.app
+{
+  width: 100px;
+  height: 100px;
+  background-color: aqua;
 }
 </style>
